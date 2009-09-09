35bb6bce77980ba0804ebaf24224bdfc18f3a1e4
jeff <jeff@rappelkiste.(none)>
2009-09-09 07:01:20 +0200

    send only to wm

--- src/screen.c
+++ src/screen.c
@@ -57,15 +57,14 @@ void
 ecompActionTerminateNotify (CompScreen *s, int plugin)
 {
 	XEvent ev;
-
 	ev.type			        = ClientMessage;
 	ev.xclient.window		= s->root;
 	ev.xclient.display      = s->display->display;
 	ev.xclient.message_type = s->display->eManagedAtom;
 	ev.xclient.format		= 32;
-	ev.xclient.data.l[0]	= ECOMORPH_ECOMP_PLUGIN_END;
-	ev.xclient.data.l[1]	= plugin;
-	ev.xclient.data.l[2]	= 0;
+	ev.xclient.data.l[0]	= 2;
+	ev.xclient.data.l[1]	= ECOMORPH_ECOMP_PLUGIN_END;
+	ev.xclient.data.l[2]	= plugin;
 	ev.xclient.data.l[3]	= 0;
 	ev.xclient.data.l[4]	= 0;
 
